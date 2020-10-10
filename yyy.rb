b49bc6b5ec38ef741efb1379a529bd14c95fca13
xdite <xdite@hey.com>
2020-10-10 20:47:52 +0800

    update

--- yyy.rb
+++ yyy.rb
@@ -1,7 +1,7 @@
 
 first_year_salary = 35000
 for i in 0..12
-  current_sarray = (first_year_salary*(1.2**(i-1))).round(2)
+  current_sarray = (first_year_salary*(1.2**(i))).round(2)
   current_year_sarray = (current_sarray * 12/10000).round(2)
   last_year_sarray = (current_year_sarray / 1.2).round(2)
   diff = (current_year_sarray-last_year_sarray).round(2)
